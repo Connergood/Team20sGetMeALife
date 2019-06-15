384a85b718f7a81f0367e088ea6f1a87e02db7cc
ahuffman <ahuffman@intelligentbits.com>
2019-06-14 23:04:34 -0400

    Basic Project Commit

---
+++ GetMeALife/GetMeALife/GetMeALife/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using GetMeALife.Models;
+
+namespace GetMeALife.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
